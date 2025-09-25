d3be19eaeaff2dea3f310f5fd3faff4f09920bc5
Chaay <chaimaae.halim@hotmail.com>
2025-09-25 19:49:23 +0100

    Add a sitemap for blog posts

---
+++ blog/sitemaps.py
@@ -0,0 +1,15 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Post
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+
+    def items(self):
+        return Post.published.all()
+
+    def lastmod(self, obj):
+        return obj.publish
+
+    def location(self, obj):
+        return obj.get_absolute_url() 
